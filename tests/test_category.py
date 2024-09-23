74a2c2127a4b131ca9bdb444ced7c78882d11301
SurPetrosian <surach2020@gmail.com>
2024-09-23 23:34:27 +0300

    тесты для д/з №2

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,10 +1,22 @@
 def test_category_init(category1, category2):
     assert category1.name == "Автомобили"
     assert category1.description == "Автомобили, как средство передвижения"
-    assert len(category1.products) == 3
+    assert len(category1.products_list) == 3
 
     assert category1.category_count == 2
     assert category2.category_count == 2
 
     assert category1.product_count == 4
     assert category2.product_count == 4
+
+
+def test_add_product(category1, product):
+    category1.add_product(product)
+    assert len(category1.products_list) == 4
+
+
+def test_products_property(category1):
+    assert category1.products == "Mercedes Bens S600, 12500000.0. " \
+                                 "Остаток: 10." \
+                                 "\nBMW X6M, 10500000.0. Остаток: " \
+                                 "5.\nAUDI A8, 9500000.0. Остаток: 12.\n"
--- tests/test_product.py
+++ tests/test_product.py
@@ -4,3 +4,28 @@ def test_product_init(product):
                                   "цвет белый, полный привод"
     assert product.price == 9500000.0
     assert product.quantity == 12
+
+
+def test_price_property(product):
+    assert product.price == 9500000.0
+
+
+def test_price_setter(product):
+    product.price = 120
+    assert product.price == 120
+    product.price = 0
+    assert "Цена не должна быть нулевая или отрицательная"
+
+
+def test_new_product(product):
+    new_product1 = product.new_product({"name": "Jaguar F-TYPE",
+                                        "description":
+                                            "Объем двигателя 4.4 литра, "
+                                        "цвет красный, задний привод",
+                                        "price": 6000000.0,
+                                        "quantity": 5})
+    assert new_product1.name == "Jaguar F-TYPE"
+    assert new_product1.description == "Объем двигателя 4.4 литра, " \
+                                       "цвет красный, задний привод"
+    assert new_product1.price == 6000000.0
+    assert new_product1.quantity == 5
